3bdea7b93d6d4689d5dc10d3213aaee75a141dc8
Syam Son Penumala <syamson132000@gmail.com>
2025-04-21 21:58:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,32 @@
-//complete this code
-class Rectangle {}
+class Rectangle {
+  constructor(width, height) {
+    this._width = width;
+    this._height = height;
+  }
 
-class Square extends Animal {}
+  get width() {
+    return this._width;
+  }
 
-// Do not change the code below this line
+  get height() {
+    return this._height;
+  }
+
+  getArea() {
+    return this._width * this._height;
+  }
+}
+
+class Square extends Rectangle {
+  constructor(side) {
+    super(side, side);
+  }
+
+  getPerimeter() {
+    return this._width * 4;
+  }
+}
+
+// ✅ Expose to window for Cypress to access
 window.Rectangle = Rectangle;
 window.Square = Square;
